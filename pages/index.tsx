f1da38923125799f0ddbcc07d9298ea98daa5d7e
writer0713 <writer0713@naver.com>
2022-12-07 14:16:00 +0900

    feat: p.96까지 완료
    - styled-components 적용
    - styled-components SSR 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,72 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-const index: NextPage = () => {
+interface Quote {
+  id: number,
+  quote: string,
+  author: string
+}
+
+interface Quotes {
+  limit: number,
+  quotes: Quote[],
+  skip: number,
+  total: number
+}
+
+interface Props {
+  data: Quotes
+}
+
+const QuoteWrapper = styled.div`
+  max-width: 400px;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px 0;
+  background-color: black;
+  color: white;
+`;
+
+const index: NextPage<Props> = ({ data }) => {
+
+  const quotes = data.quotes;
+
+  // const [quotes, setQuotes] = useState<Quote[]>([]);
+
+  // const getQuotes = async () => {
+  //   const res = await fetch(`https://dummyjson.com/quotes`);
+  //   return await res.json();
+  // };
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const result: Quotes = await getQuotes();
+  //     console.log(result);
+  //     const data: Quote[] = result.quotes;
+  //     setQuotes(data);
+  //   })();
+  // }, []);
+
+  const quotesElem = quotes.map((quote: Quote) => <QuoteWrapper key={quote.id}>
+    <p>id : {quote.id}</p>
+    <p>quote : {quote.quote}</p>
+    <p>author : {quote.author}</p>
+  </QuoteWrapper>)
   return (
-    <div>
-      Enter
-    </div>
+    <>
+      {quotesElem}
+    </>
   );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://dummyjson.com/quotes`)
+  const data: Quotes = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
 
 export default index;
